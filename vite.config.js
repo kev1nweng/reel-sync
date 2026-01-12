d330c9758ed37f3e8cf4d243d6e71c7a73f272be
Kevin WENG <kw@kev1nweng.space>
2026-01-11 19:09:51 -0500

    chore: google adsense meta

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,43 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 import { version } from "./package.json";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => tag.startsWith("mdui-"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: (tag) => tag.startsWith("mdui-"),
+          },
+        },
+      }),
+      {
+        name: "html-transform",
+        transformIndexHtml(html) {
+          const adsenseId = env.VITE_ADSENSE_ACCOUNT;
+          if (adsenseId) {
+            return html.replace(
+              "<head>",
+              `<head>\n    <meta name="google-adsense-account" content="${adsenseId}">`,
+            );
+          }
+          return html;
         },
       },
-    }),
-  ],
-  define: {
-    __APP_VERSION__: JSON.stringify(version),
-  },
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    ],
+    define: {
+      __APP_VERSION__: JSON.stringify(version),
     },
-  },
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  };
 });
