ed853ae003e5823745f0c1d7a2c1160ab9b50977
Kevin WENG <kw@kev1nweng.space>
2026-02-04 11:47:36 -0500

    feat(VideoInput): 修复引入pinia和ref直接引用后的事件转发

--- src/components/VideoInput.vue
+++ src/components/VideoInput.vue
@@ -1,9 +1,28 @@
 <template>
-  <input type="file" accept="video/*" style="display: none" />
+  <input ref="input" type="file" accept="video/*" style="display: none" @change="onChange" />
 </template>
 
 <script>
 export default {
   name: "VideoInput",
+  emits: ["change"],
+  methods: {
+    click() {
+      this.$refs.input?.click();
+    },
+    onChange(event) {
+      this.$emit("change", event);
+    },
+  },
+  computed: {
+    value: {
+      get() {
+        return this.$refs.input?.value ?? "";
+      },
+      set(nextValue) {
+        if (this.$refs.input) this.$refs.input.value = nextValue ?? "";
+      },
+    },
+  },
 };
 </script>
